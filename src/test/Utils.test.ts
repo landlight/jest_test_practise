071910fef5abae1e2874a9d287ccb6dae2004724
Amity <amity@Light-MacBook-Pro.local>
2022-11-09 10:31:28 +0700

    :white_check_mark: add object assertions

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -11,5 +11,15 @@ describe('Utils test suite', () => {
         expect(parsedUrl.href).toBe('http://localhost:8080/login');
         expect(parsedUrl.port).toBe('8080');
         expect(parsedUrl.protocol).toBe('http:');
+        expect(parsedUrl.query).toEqual({});
+    })
+
+    test('parse URL with query', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:8080/login?user=user&password=pass');
+        const expectedQuery = {
+            user: 'user',
+            password: 'pass'
+        }
+        expect(parsedUrl.query).toEqual(expectedQuery);
     })
 });
