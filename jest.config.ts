57f58ecd8912d51f5c105034e8ae345c4309d538
landlight <ck.light@gmail.com>
2022-11-15 21:39:53 +0700

    fix coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,14 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-    roots: ['<rootDir>/src'],
+    roots: ['<rootDir>/src/test'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest'
     },
     testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     verbose: true,
-    collectCoverage: false,
+    collectCoverage: true,
     collectCoverageFrom: ['<rootDir>/src/app/**/*.ts']
 }
 
